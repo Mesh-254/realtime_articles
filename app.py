a506f13d54cd18d2833801b7f169040b0979c529
Mesh-254 <meshack1995@gmail.com>
2024-03-21 08:54:29 +0300

    updates

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
 from flask import *
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models.database import db
+from flask_login import LoginManager, login_required, current_user
+
+from blueprints.user import user
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
+app.register_blueprint(user, url_prefix='/')
+
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+
+# Initialize the SQLAlchemy instance with the Flask app
+db.app = app
+db.init_app(app)
+
 migrate = Migrate(app, db)
 
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'user.login'
 
-from models import User
+# Import User model after initializing SQLAlchemy
+from models.models import User
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'realtime_articles.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # To suppress SQLAlchemy warning
