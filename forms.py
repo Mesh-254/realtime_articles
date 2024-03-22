f3847431eabdd2bf3f0d663f9b9e6feb12a9a4aa
Mesh-254 <meshack1995@gmail.com>
2024-03-22 18:47:02 +0300

    created forms to handle category inputs

--- forms.py
+++ forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, ValidationError
-from models.models import User
 
 
 class UserLoginForm(FlaskForm):
     email_or_username = StringField('Email or Username', validators=[DataRequired(), Length(max=100)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     remember_me = BooleanField('Remember Me')
+
+
+class CategoryForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=50)])
+    description = TextAreaField('Description', render_kw={"rows": 6})  # Set rows attribute to 4
