2269be973f1b752c3fa6e3e1f1552150fc7c1953
Mesh-254 <meshack1995@gmail.com>
2024-03-26 19:08:13 +0300

    updated user login function to accept only admins

--- blueprints/user.py
+++ blueprints/user.py
@@ -11,32 +11,27 @@ user = Blueprint('user', __name__)
 @user.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('user.index'))
+        return redirect(url_for('article.base'))
 
     form = UserLoginForm()
     if form.validate_on_submit():
-        # Try to find user by username
+        # Try to find user by username or email
         user = User.query.filter(or_(User.username == form.email_or_username.data,
                                      User.email == form.email_or_username.data)).first()
         # Check if user exists and password matches
         if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get('next')
-            # Check if there's a next page or if it's a valid URL
-            if not next_page or urlsplit(next_page).netloc != '':
-                next_page = url_for('user.index')
+            if user.is_admin:  # Check if the user is an admin
+                login_user(user, remember=form.remember_me.data)
+                next_page = request.args.get('next')
+                # Check if there's a next page or if it's a valid URL
+                if not next_page or urlsplit(next_page).netloc != '':
+                    next_page = url_for('article.base')
                 return redirect(next_page)
+            else:
+                flash('Only admins are allowed to log in.', 'danger')
         else:
             flash('Invalid username or password', 'danger')
-            return redirect(url_for('user.login'))
     return render_template('login/login.html', form=form)
-
-
-@user.route('/')
-@user.route('/index')
-def index():
-    return render_template('home/blog_list.html', title='Home')
-
 
 @user.route('/logout')
 @login_required
