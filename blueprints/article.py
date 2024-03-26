1b8a4f0fa47d3f6b4311fbdd0b9e53d4211e885d
Mesh-254 <meshack1995@gmail.com>
2024-03-26 19:04:51 +0300

    updated and created functions to handle rendering of articles

--- blueprints/article.py
+++ blueprints/article.py
@@ -1,7 +1,10 @@
+import os
 from flask import *
-from flask_login import login_required
+from flask_login import login_required, current_user
+from werkzeug.utils import secure_filename
+
 from forms import CategoryForm, ArticleForm
-from models.models import Category, Article
+from models.models import Category, Article, User
 from models.database import db
 
 article = Blueprint('article', __name__)
@@ -89,37 +92,117 @@ def delete_category(id):
     return render_template('admin/category/delete_category.html', category=category)
 
 
-@article.route('/create_article')
+@article.route('/create_article', methods=['GET', 'POST'])
 @login_required
 def create_article():
+    title = 'Create Article / Blog'
     form = ArticleForm()
     if form.validate_on_submit():
         # Check if an article with the same title already exists
         existing_article = Article.query.filter_by(main_title=form.main_title.data).first()
         if existing_article:
             flash("Article with the same title already exists!", 'danger')
-            return redirect(url_for('create_article'))
+            return redirect(url_for('article.create_article'))
+
+        main_image = form.main_image.data  # Save uploaded image and get its file path
+
+        if main_image.filename != '':
+            uploaded_file = secure_filename(main_image.filename)
+            file_ext = os.path.splitext(uploaded_file)[1]
+            if file_ext not in current_app.config['UPLOAD_EXTENSIONS']:
+                flash('File type not allowed. Please upload a different file type.', 'danger')
+                return redirect(url_for('article.create_article'))
+            main_image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], uploaded_file))
+        else:
+            uploaded_file = None
 
         # Create a new article instance and populate its fields from the form
         new_article = Article(
             main_title=form.main_title.data,
             main_content=form.main_content.data,
-            author_id=form.author_id.data,
             category_id=form.category_id.data,
-            main_image=form.main_image.data
+            main_image=uploaded_file,
+            author_id=form.author_id.data
         )
-
         # Add the new article to the database session and commit changes
         db.session.add(new_article)
         db.session.commit()
-
-        flash("Article created successfully!", 'success')
+        flash(" Your Article/Blog was created successfully!", 'success')
         return redirect(url_for('article.article_list'))  # Redirect to the homepage or any other relevant page
+    return render_template('admin/articles/edit_article.html', form=form, title=title)
 
-    return render_template('create_article.html', form=form, title='Create Article/Blog')
 
 
+
+# Fetch category and author details in your article_list function
 @article.route('/article_list')
 @login_required
 def article_list():
-    pass
+    title = 'Article/Blog List'
+    articles = Article.query.order_by(Article.date_published.desc()).all()
+
+    # Fetch category and author details for each article
+    for article in articles:
+        category = Category.query.get(article.category_id)
+        author = User.query.get(article.author_id)
+        article.category_name = category.name if category else 'N/A'
+        article.author_name = author.username if author else 'N/A'
+
+    return render_template('admin/articles/article_list.html', title=title,
+                           articles=articles)
+
+
+@article.route('/update_article/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def update_article(article_id):
+    title = 'Update title'
+    article = Article.query.get_or_404(article_id)
+    form = ArticleForm(obj=article)
+    if form.validate_on_submit():
+        main_image = form.main_image.data
+        if main_image:
+            uploaded_file = secure_filename(main_image.filename)
+            file_ext = os.path.splitext(uploaded_file)[1]
+            if file_ext not in current_app.config['UPLOAD_EXTENSIONS']:
+                flash('File type not allowed. Please upload a different file type.', 'danger')
+                return redirect(url_for('article.create_article', article_id=article_id))
+            main_image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], uploaded_file))
+        else:
+            uploaded_file = None
+        # If editing existing article, update its details
+        article.main_title = form.main_title.data
+        article.main_content = form.main_content.data
+        article.category_id = form.category_id.data
+        if uploaded_file:
+            article.main_image = uploaded_file
+        article.author_id = form.author_id.data
+        db.session.commit()
+        flash("Article updated successfully!", 'success')
+        return (redirect(url_for('article.article_list')))
+    return render_template('admin/articles/edit_article.html', form=form, title=title)
+
+
+@article.route('/delete_article/<int:article_id>', methods=['GET', 'POST'])
+@login_required
+def delete_article(article_id):
+    # Retrieve the article from the database
+    article = Article.query.get_or_404(article_id)
+    title = 'Delete Article'
+    if request.method == 'POST':
+
+        # Check if the article has an associated image and delete it from the server
+        if article.main_image:
+            image_filename = secure_filename(article.main_image)
+            image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image_filename)
+            if os.path.exists(image_path):
+                os.remove(image_path)
+
+        # Delete the article from the database
+        db.session.delete(article)
+        db.session.commit()
+
+        # Provide feedback to the user
+        flash("Article deleted successfully!", "success")
+        # Redirect the user to a relevant page
+        return redirect(url_for('article.article_list'))
+    return render_template('admin/articles/delete_article.html', title=title)
