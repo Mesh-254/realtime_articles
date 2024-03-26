e4a4be660fbcc0d7aacca3154f706be4f8c9bcf3
Mesh-254 <meshack1995@gmail.com>
2024-03-26 19:10:56 +0300

    updated config file to accept uploads

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'realtime_articles.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # To suppress SQLAlchemy warning
+    UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif', '.jpeg']
+    UPLOAD_FOLDER = 'static/uploads'
