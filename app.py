273dec2de46cb1b48956b95277c343ddf220c0b0
Mesh-254 <meshack1995@gmail.com>
2024-03-26 19:03:45 +0300

    made updates to the app file

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
+import os
+from dotenv import load_dotenv
 from flask import *
+from werkzeug.utils import secure_filename
+
 from config import Config
 from flask_migrate import Migrate
+
+from forms import ArticleForm
 from models.database import db
 from flask_login import LoginManager, login_required, current_user
 
 from blueprints.user import user
 from blueprints.article import article
+from blueprints.home import home
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 app.url_map.strict_slashes = False
 
+app.register_blueprint(home, url_prefix='/')
 app.register_blueprint(user, url_prefix='/')
 app.register_blueprint(article, url_prefix='/')
 
+# Importing of config variables
 app.config.from_object(Config)
 
 # Initialize the SQLAlchemy instance with the Flask app
@@ -25,15 +34,15 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.init_app(app)
 login.login_view = 'user.login'
+load_dotenv()
 
 # Import User model after initializing SQLAlchemy
-from models.models import User
+from models.models import User, Article
 
 
 @login.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 
 if __name__ == '__main__':
