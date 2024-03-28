b43ca18ecab891983b2626e15dd4fcf52515d523
Mesh-254 <meshack1995@gmail.com>
2024-03-28 18:51:16 +0300

    created method to fetch article and subsequent heading details

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,5 +1,5 @@
 from flask import *
-from models.models import Article
+from models.models import Article, Subheading, ThirdLevelSubheading
 
 home = Blueprint('home', __name__)
 
@@ -10,6 +10,20 @@ def index():
     title = 'Realtime Articles'
     articles = Article.query.order_by(Article.date_published.desc()).all()
     return render_template('home/blog_list.html', articles=articles, title=title)
+
+
+
+
+@home.route('/article_details/<int:id>')
+def article_details(id):
+    article= Article.query.get_or_404(id)
+    subheadings = Subheading.query.filter_by(article_id=id).all()
+    third_level_subheadings = []
+    for subheading in subheadings:
+        third_level_subheadings.extend(ThirdLevelSubheading.query.filter_by(subheading_id=subheading.id).all())
+        print(third_level_subheadings)
+    return render_template('home/article_detail.html',
+                           article=article, subheadings=subheadings, third_level_subheadings=third_level_subheadings)
 
 
 @home.route('/uploads/<filename>')
