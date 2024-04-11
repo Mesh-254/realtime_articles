8a5a0fbb3db30bd41ccbfaa27a2d29e2426c75d8
Mesh-254 <meshack1995@gmail.com>
2024-04-11 11:36:46 +0300

    updated forms to only contain article and category forms

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, TextAreaField, SelectField, FileField
 from wtforms.validators import DataRequired, Length
-from models.models import User, Category, Article,Subheading
+from models.models import User, Category, Article
 from flask_ckeditor import CKEditorField
 
 
@@ -27,25 +27,3 @@ class ArticleForm(FlaskForm):
         super(ArticleForm, self).__init__(*args, **kwargs)
         self.author_id.choices = [(author.id, author.username) for author in User.query.all()]
         self.category_id.choices = [(category.id, category.name) for category in Category.query.all()]
-
-
-class SubheadingForm(FlaskForm):
-    sub_title = StringField('Subheading Title', validators=[DataRequired()])
-    sub_content = TextAreaField('Subheading Content', validators=[DataRequired()], render_kw={"rows": 7})
-    sub_image = FileField('Subheading Image')  # Assuming it's a path
-    article_id = SelectField('Article', coerce=int)  # Select field for article
-
-    def __init__(self, *args, **kwargs):
-        super(SubheadingForm, self).__init__(*args, **kwargs)
-        self.article_id.choices = [(article.id, article.main_title) for article in Article.query.all()]
-
-
-class ThirdLevelSubheadingForm(FlaskForm):
-    sub_title = StringField('Subheading Title')
-    sub_content = TextAreaField('Subheading Content', render_kw={"rows": 6})
-    sub_image = FileField('Subheading Image')
-    subheading_id = SelectField('Subheading', coerce=int, validators=[DataRequired()])
-
-    def __init__(self, *args, **kwargs):
-        super(ThirdLevelSubheadingForm, self).__init__(*args, **kwargs)
-        self.subheading_id.choices = [(subheading.id, subheading.sub_title) for subheading in Subheading.query.all()]
