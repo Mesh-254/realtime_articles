aae57a872b21274ce294dba5d1efbc820c3fbecf
Mesh-254 <meshack1995@gmail.com>
2024-04-11 11:37:30 +0300

    created a Truncate main content of each article to 50 words

--- blueprints/home.py
+++ blueprints/home.py
@@ -1,10 +1,10 @@
 from flask import *
-from models.models import Article, Subheading, ThirdLevelSubheading
+from models.models import Article
 
 home = Blueprint('home', __name__)
 
 
-@home.route('/uploads/<filename>')
+@home.route('/<filename>')
 def get_uploaded_image(filename):
     return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
 
@@ -22,16 +22,10 @@ def index():
 
 
 
-
 @home.route('/article_details/<int:id>')
 def article_details(id):
     article= Article.query.get_or_404(id)
-    subheadings = Subheading.query.filter_by(article_id=id).all()
-    third_level_subheadings = []
-    for subheading in subheadings:
-        third_level_subheadings.extend(ThirdLevelSubheading.query.filter_by(subheading_id=subheading.id).all())
-        print(third_level_subheadings)
     return render_template('home/article_detail.html',
-                           article=article, subheadings=subheadings, third_level_subheadings=third_level_subheadings)
+                           article=article)
 
 
