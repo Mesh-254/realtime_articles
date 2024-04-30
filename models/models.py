2d0a0529964011685dedab5bc6ed27c60824892f
Mesh-254 <meshack1995@gmail.com>
2024-04-30 20:47:54 +0300

    included image colum to category model

--- models/models.py
+++ models/models.py
@@ -35,9 +35,10 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     description = db.Column(db.Text)
+    image = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return '<Category {}>'.format(self.name)
+        return '<Category {}>'.format(self.name, self.image)
 
 
 class Article(db.Model):
