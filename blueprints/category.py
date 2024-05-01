6f63c3ea19142f71269ecb3b2145e04f7c1d48b2
Mesh-254 <meshack1995@gmail.com>
2024-05-01 13:34:01 +0300

    created method to get articles_by_category

--- blueprints/category.py
+++ blueprints/category.py
@@ -1,7 +1,8 @@
 import random
-
+import re
 from flask import *
 from models.models import *
+
 category = Blueprint('category', __name__)
 
 
@@ -11,7 +12,7 @@ def categories():
 
     # List of icon classes
     icon_badges = [
-        "badge text-bg-warning", "badge text-bg-success", "badge bg-dark", "badge bg-primary"," badge text-bg-info"
+        "badge text-bg-warning", "badge text-bg-success", "badge bg-dark", "badge bg-primary", " badge text-bg-info"
 
     ]
     # Randomly select an icon class
@@ -23,3 +24,36 @@ def categories():
             query.description = ' '.join(text[:30])
 
     return render_template('/category/category.html', queryset=queryset, random_badge_class=random_badge_class)
+
+
+@category.route('/category/<int:category_id>/articles', methods=['GET'])
+def get_articles_by_category(category_id):
+    category = Category.query.get_or_404(category_id)
+
+    articles = Article.query.filter_by(category_id=category_id).all()
+
+    reading_time = 0  # Default value for reading time
+
+    # limiting the number of content to be displayed on screen
+    for query in articles:
+
+        # Calculates the number of words in the article content
+        word_count = len(re.findall(r'\w+', query.main_content))
+        # Calculate the estimated reading time
+        reading_time = round(word_count / 200)  # average reading time is 200 words per minute
+
+        text = query.main_content.split()
+        if len(text) > 10:
+            query.main_content = ' '.join(text[:50])
+
+    # List of icon classes
+    icon_badges = [
+            "badge text-bg-warning", "badge text-bg-success", "badge bg-dark", "badge bg-primary", " badge text-bg-info"
+
+    ]
+    # Randomly select an icon class
+    random_badge_class = random.choice(icon_badges)
+
+    return render_template('/category/articles_by_category_list.html',
+                           articles=articles, category=category, reading_time=reading_time,
+                           random_badge_class=random_badge_class)
